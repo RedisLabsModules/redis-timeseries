e8945d123ae3fb491016056a2d5b509dddefb737
Danni Moiseyev <danni@redislabs.com>
2021-03-17 11:48:37 +0200

    wip

--- tests/flow/test_with_gears.sh
+++ tests/flow/test_with_gears.sh
@@ -3,7 +3,7 @@ apt-get install git python build-essential
 python3 -m pip install -r ./requirements.txt
 
 mkdir -p /tmp/gears
-cd /tmp/gears
+pushd /tmp/gears
 
 git clone https://github.com/RedisGears/RedisGears.git
 cd RedisGears
@@ -14,6 +14,8 @@ git submodule update
 make setup
 make fetch
 make all
+
+pod
 
 python3 -m RLTest --clear-logs --env oss-cluster --shards-count 4 \
   --module ../../bin/redistimeseries.so \
